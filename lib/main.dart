c852eb9423fec254a2234118e9ad2c8a5a64a238
Helena Petri <helenapetri@Helenas-MacBook-Pro.local>
2022-08-09 21:27:14 -0600

    Added UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart' as hooks;
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+import 'package:reduxexample/enums.dart';
+import 'package:reduxexample/redux/actions.dart';
+import 'package:reduxexample/redux/reducers.dart';
+import 'package:reduxexample/redux/state.dart' as state;
 
 void main() {
   runApp(MaterialApp(
@@ -11,13 +18,92 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends hooks.HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final store = Store(
+      appStateReducer,
+      initialState: const state.State(
+        items: [],
+        filter: ItemFilter.all,
+      ),
+    );
+    final textController = hooks.useTextEditingController();
     return Scaffold(
       appBar: AppBar(title: const Text('Home Page')),
+      body: StoreProvider(
+        store: store,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    store
+                        .dispatch(const ChangeFilterTypeAction(ItemFilter.all));
+                  },
+                  child: const Text('All'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    store.dispatch(
+                        const ChangeFilterTypeAction(ItemFilter.shortTexts));
+                  },
+                  child: const Text('Short items'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    store.dispatch(
+                        const ChangeFilterTypeAction(ItemFilter.longTexts));
+                  },
+                  child: const Text('Long items'),
+                )
+              ],
+            ),
+            TextField(
+              controller: textController,
+            ),
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    final text = textController.text;
+                    store.dispatch(AddItemAction(text));
+                    textController.clear();
+                  },
+                  child: const Text('Add'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    final text = textController.text;
+                    store.dispatch(RemoveItemAction(text));
+                    textController.clear();
+                  },
+                  child: const Text('Remove'),
+                ),
+              ],
+            ),
+            StoreConnector<state.State, Iterable<String>>(
+              converter: (store) => store.state.filteredItems,
+              builder: (context, items) {
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      final item = items.elementAt(index);
+                      return ListTile(
+                        title: Text(item),
+                      );
+                    },
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/redux/reducers.dart
+++ lib/redux/reducers.dart
@@ -3,12 +3,9 @@ import 'package:reduxexample/redux/actions.dart';
 import 'package:reduxexample/extensions/add_remove_tems.dart';
 import 'package:redux/redux.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:reduxexample/redux/state.dart';
 
-Reducer<Iterable<String>> itemsReducer = combineReducers<Iterable<String>>([
-  TypedReducer<Iterable<String>, AddItemAction>(addItemReducer),
-  TypedReducer<Iterable<String>, RemoveItemAction>(removeItemReducer)
-]);
-
+// For items
 Iterable<String> addItemReducer(
         Iterable<String> previousItems, AddItemAction action) =>
     previousItems + action.item;
@@ -17,7 +14,25 @@ Iterable<String> removeItemReducer(
         Iterable<String> previousItems, RemoveItemAction action) =>
     previousItems - action.item;
 
+Reducer<Iterable<String>> itemsReducer = combineReducers<Iterable<String>>([
+  TypedReducer<Iterable<String>, AddItemAction>(addItemReducer),
+  TypedReducer<Iterable<String>, RemoveItemAction>(removeItemReducer)
+]);
+
+// For filters
 ItemFilter itemFilterReducer(
   State oldState,
   Action action,
-) {}
+) {
+  if (action is ChangeFilterTypeAction) {
+    return action.filter;
+  } else {
+    return oldState.filter;
+  }
+}
+
+// App reducer
+State appStateReducer(State oldState, action) => State(
+      items: itemsReducer(oldState.items, action),
+      filter: itemFilterReducer(oldState, action),
+    );
